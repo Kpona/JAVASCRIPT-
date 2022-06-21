3a4bf42c5c0302851235ad14a94f1b827766097c
Kpona <105780570+Kpona@users.noreply.github.com>
2022-06-21 18:48:29 +0100

    JAVASCRIPT-

---
+++ script.js
@@ -0,0 +1,17 @@
+let height="5'11";
+let name='ISIAKPONA DANIEL';
+let country='Nigeria';
+
+console.log("I am known as "  + name);
+
+
+console.log("I am " +height +"feet tall");
+
+
+console.log("I am from "+ country)
+
+document.write("<h2>NAME:-<br>My name is ISIAKPONA DANIEL</h2>");
+
+document.write("<h3>HEIGHT:-<br>I am 5'11feet tall</h3>");
+
+document.write("<h4>COUNTRY:-<br>I am from NIGERIA</h4>");
